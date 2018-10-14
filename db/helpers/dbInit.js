c36b54dbaeff762c52f44800b6142058a8d24385
Sebastiano <sebalavoro@gmail.com>
2018-10-14 18:47:26 +0200

    fix(): init error cause by mongo trying to drop non existing collection

--- db/helpers/dbInit.js
+++ db/helpers/dbInit.js
@@ -3,10 +3,12 @@ const readFileSync = require("fs").readFileSync;
 
 async function dbInit() {
     await db.connect();
-    await db.dropCampaigns();
-    const initData = await readFileSync('./resources/data.json');
-    const parsedInitData = JSON.parse(initData);
-    await db.seedCampaigns(parsedInitData);
+    const campaigns = await db.getAllCampaigns();
+    if (campaigns.length<=0) {
+        const initData = await readFileSync('./resources/data.json');
+        const parsedInitData = JSON.parse(initData);
+        await db.seedCampaigns(parsedInitData);
+    }
     await db.disconnect();
 }
 
